978a1f421325b771be6e83355de1b166f255ec99
BEANANDMENTOS <72437476+BEANANDMENTOS@users.noreply.github.com>
2023-04-18 13:33:40 +0700

    first push

---
+++ aws/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+#provider block
+provider "aws" {
+  region = "ap-southeast-1"
+  # profile = "default"
+}
+
+resource "aws_s3_bucket" "static_react_bucket" {
+  bucket = "poorin-bucket"
+  acl    = "private"
+
+  tags = {
+    Name = "poorin-bucket"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.static_react_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.static_react_bucket.bucket_regional_domain_name
+    # origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  # logging_config {
+  #   include_cookies = false
+  #   bucket          = ".amazonaws.com"
+  #   prefix          = "myprefix"
+  # }
+
+  # aliases = ["mysite.example.com", "yoursite.example.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "TH"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
